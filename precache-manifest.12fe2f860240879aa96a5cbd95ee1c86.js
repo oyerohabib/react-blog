00e601272faec1a37f8079cb11809cbbf19a0afc
oyero habib <oyerohabib@yahoo.com>
2020-07-18 15:39:39 -0700

    Updates

---
+++ precache-manifest.12fe2f860240879aa96a5cbd95ee1c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d6bfa777ffa2fa8ab29720c924e9bd6",
+    "url": "/react-blog/index.html"
+  },
+  {
+    "revision": "38bee8caf96b34b038f2",
+    "url": "/react-blog/static/css/main.a9e848bd.chunk.css"
+  },
+  {
+    "revision": "682a448458a8a9f8269a",
+    "url": "/react-blog/static/js/2.6c31db10.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-blog/static/js/2.6c31db10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38bee8caf96b34b038f2",
+    "url": "/react-blog/static/js/main.31e4d3e3.chunk.js"
+  },
+  {
+    "revision": "bd6dc07a134fbf0c4903",
+    "url": "/react-blog/static/js/runtime-main.9fb7c4e9.js"
+  },
+  {
+    "revision": "efd5afbb97325a0c5cce97669760e51a",
+    "url": "/react-blog/static/media/best6.efd5afbb.png"
+  }
+]);
